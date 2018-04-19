7f5e84df48f961f2467524f3c4f902ca7fc631f6
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2018-04-19 15:18:17 +0300

    redirection fixed

--- carrier/urls.py
+++ carrier/urls.py
@@ -5,12 +5,12 @@ from carrier.views import ItemListView
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-     url(r'^itemlist/$', views.ItemListView.as_view()),
+     url(r'^itemlist/$', views.ItemListView.as_view(),name='item-list'),
      url(r'^profile/$', views.MyProfile.as_view()),
       url(r'^responsibilities/$', views.MyResponsibilities.as_view()),
       url(r'^aboutus/', views.AboutUs.as_view()),
       #url(r'^newpost/$', views.NewPost.as_view(), name='add_post'),
-       url(r'^product/entry/$',views.ProductEntry.as_view(),name='product-entry'),
+       url(r'^item/entry/$',views.ProductEntry.as_view(),name='product-entry'),
      
 
 ]
